0810894d69026c8008678fc6382ecbe58ec50fd2
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-01-31 08:53:29 +0900

    template

--- blog.go
+++ blog.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"html/template"
+	"io"
 
+	"github.com/labstack/echo/v4"
+)
+
+//Template wrapper
+type Template struct {
+	templates *template.Template
+}
+
+//Render template
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func main() {
+	t := &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+	e := echo.New()
+	e.Renderer = t
+	e.Static("/public", "public")
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(
+			200,
+			"index.html",
+			map[string]interface{}{
+				"navtitle": "open lemon blog",
+				"gbody":    "",
+				"ghead":    "",
+				"gfooter":  "",
+
+				"body":        "",
+				"tag":         []string{},
+				"ogimg":       false,
+				"description": "",
+				"title":       "",
+				"section":     "",
+			},
+		)
+	})
+	e.Logger.Fatal(e.Start(":19601"))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lemon-mint/openlemonblog
 
 go 1.15
+
+require github.com/labstack/echo/v4 v4.1.17
